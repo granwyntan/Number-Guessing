a112324f2fa7497ef57bbd3264ea4d53e99ccc43
Granwyn Tan <61010627+granwyntan@users.noreply.github.com>
2020-10-15 21:01:35 +0800

    Add files via upload

---
+++ numberGuessing.js
@@ -0,0 +1,108 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function print(x){
+//   console.log(x);
+// }
+// const print = console.log
+
+var choices = [];
+
+const prompt = require('prompt-sync')({sigint: true});
+
+var end = false;
+
+while (!end) {
+  // console.log('Only Positive Integers Accepted');
+  let rangeMin = prompt('Minimum Number (Default - 1): ');
+  let rangeMax = prompt('Maximum Number (Default - 100): ');
+
+  if (!isNaN(Number(rangeMin)) && !isNaN(Number(rangeMax)) && rangeMin && rangeMax){
+    if (rangeMin == rangeMax) {
+      rangeMin = null;
+      rangeMax = null;
+      console.log('Maximum Number is equal to Minimum Number');
+      console.log('Minimum Number is equal to Maximum Number');
+      console.log('Numbers set to default');
+    } else if (rangeMin > rangeMax) {
+      rangeMin = null;
+      rangeMax = null;
+      console.log('Maximum Number is smaller than Minimum Number');
+      console.log('Minimum Number is greater than Maximum Number');
+      console.log('Numbers set to default');
+    }
+  } else if (isNaN(rangeMin) || isNaN(rangeMax) || !rangeMax || !rangeMin) {
+    console.log('Either one or both numbers are invalid');
+    console.log('Invalid Number(s) set to default');
+  }
+
+  if (!rangeMin || isNaN(rangeMin)) {
+    rangeMin = 1;
+  }
+  if (!rangeMax || isNaN(rangeMax)) {
+    rangeMax = 100;
+  }
+
+  var tries = 0;
+  // Random number from 1 - 10
+  let numberToGuess = getRandomInt(rangeMin, rangeMax);
+  // This variable is used to determine if the app should continue prompting the user for input
+  let foundCorrectNumber = false;
+  // console.log(numberToGuess);
+
+  while (!foundCorrectNumber) {
+    tries += 1;
+    // Get user input
+    var guess = prompt(`Guess a number from ${rangeMin} to ${rangeMax}: `);
+    // Compare the guess to the secret answer and let the user know.
+    if (guess) {
+      // if (typeof Number(guess) === "number" || Number(guess) !== NaN || Number(guess) != NaN){
+      if (!isNaN(Number(guess))) {
+        guess = Number(guess);
+        // console.log(guess);
+        // console.log(Number(guess));
+        // console.log(choices);
+        // console.log(choices.includes(guess));
+        if (guess === numberToGuess || guess == numberToGuess) {
+          console.log('Congrats, you got it!');
+          foundCorrectNumber = true;
+        } else if (choices.includes(guess)) {
+          console.log('Number has been guessed before');
+        } else if (guess > rangeMax || guess < rangeMin) {
+          console.log('Out of Range');
+        } else if (guess > numberToGuess) {
+          console.log('Sorry, guess again!\nNumber is Smaller');
+        } else if (guess < numberToGuess) {
+          console.log('Sorry, guess again!\nNumber is Larger');
+        } 
+        choices.push(guess);
+      } else {
+        console.log('Invalid Input');
+      }
+    } else {
+      console.log('Invalid Input');
+    }
+  }
+
+  console.log(`The number was ${numberToGuess}`);
+  
+  if (tries > 1) {
+    console.log(`You tried ${tries} times`);
+  } else {
+    console.log('You tried 1 time');
+  }
+
+  let choice = prompt('Again [Y/N]? ');
+  if (choice.toLowerCase().includes('n') || choice.toLowerCase().includes('x') || Boolean(choice) == false) {
+    end = true;
+  } else if (choice.toLowerCase().includes('y') || Boolean(choice) == true) {
+    end = false;
+    console.log();
+    choices = [];
+  } else {
+    end = true;
+  }
+}
---
+++ numberGuessing.swift
@@ -0,0 +1,99 @@
+import Foundation 
+var choices: [Int] = []
+var end = false 
+while !end { 
+    print("Minimum Number (Default - 1)", terminator: ": ") 
+    var rangeMinInput = readLine()
+    print("Maximum Number (Default - 100)", terminator: ": ")
+    var rangeMaxInput = readLine()
+    var rangeMin = Int()
+    var rangeMax = Int()
+    if Int(rangeMinInput!) != nil && Int(rangeMaxInput!) != nil && rangeMaxInput != nil && rangeMinInput != nil {
+        if Int(rangeMinInput!) == Int(rangeMaxInput!) {
+            rangeMinInput = nil 
+            rangeMaxInput = nil 
+            print("Maximum Number is equal to Minimum Number")
+            print("Minimum Number is equal to Maximum Number")
+            print("Numbers set to default") 
+        } else if Int(rangeMinInput!)! > Int(rangeMaxInput!)! { 
+            rangeMinInput = nil 
+            rangeMaxInput = nil 
+            print("Maximum Number is smaller than Minimum Number") 
+            print("Minimum Number is greater than Maximum Number") 
+            print("Numbers set to default") 
+        }
+    } else if Int(rangeMinInput!) != nil || Int(rangeMaxInput!) != nil || rangeMinInput?.isEmpty == true || rangeMaxInput?.isEmpty == true { 
+        print("Either one or both numbers are invalid") 
+        print("Invalid Number(s) set to default") 
+    } 
+    
+    if let rangeMinInput = rangeMinInput { 
+        if rangeMinInput.isEmpty == true || Int(rangeMinInput) == nil { 
+            rangeMin = 1 
+        } else { 
+            rangeMin = Int(rangeMinInput)! 
+        } 
+    } else { 
+        rangeMin = 1 
+    }   
+    if let rangeMaxInput = rangeMaxInput { 
+        if rangeMaxInput.isEmpty == true || Int(rangeMaxInput) == nil { 
+            rangeMax = 100 
+        } else { 
+            rangeMax = Int(rangeMaxInput)! 
+        } 
+    } else { 
+        rangeMax = 100 
+    } 
+    var tries = 0
+    let numberToGuess = Int.random(in: rangeMin...rangeMax) 
+    var foundCorrectNumber = false 
+    while !foundCorrectNumber { 
+        tries += 1 
+        print("Guess a number from \(rangeMin) to \(rangeMax)", terminator: ": ") 
+        let guessInput = readLine() 
+        if !guessInput!.isEmpty { 
+            if Int(guessInput!) != nil { 
+                let guess = Int(guessInput!) 
+                // print(guess) 
+                // print(Number(guess)) 
+                // print(choices) 
+                // print(choices.contains(guess)) 
+                if guess == numberToGuess { 
+                    print("Congrats, you got it!") 
+                    foundCorrectNumber = true 
+                } else if (choices.contains(guess!)) { 
+                    print("Number has been guessed before") 
+                } else if (guess! > rangeMax || guess! < rangeMin) { 
+                    print("Out of Range") 
+                } else if (guess! > numberToGuess) { 
+                    print("Sorry, guess again!\nNumber is Smaller") 
+                } else if (guess! < numberToGuess) { 
+                    print("Sorry, guess again!\nNumber is Larger") 
+                } 
+                choices.append(guess!) 
+            } else { 
+                print("Invalid Input") 
+            } 
+        } else { 
+            print("Invalid Input") 
+        } 
+    } 
+    print("The number was \(numberToGuess)") 
+    if (tries > 1) { 
+        print("You tried \(tries) times") 
+    } else { 
+        print("You tried 1 time") 
+    } 
+    print("Again [Y/N]", terminator: "? ") 
+    let choice = readLine() 
+    if choice!.lowercased().contains("n") || choice!.lowercased().contains("x") || Bool(choice!) == false { 
+        end = true 
+    } else if choice!.lowercased().contains("y") || Bool(choice!) == true { 
+        end = false 
+        print() 
+        choices = [] 
+    } else { 
+        end = true 
+    } 
+}
